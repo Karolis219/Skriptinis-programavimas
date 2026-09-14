369071c8664dee3fd010f976d9cc1fc5e00cc13d
Karolis219 <k.juodzevicius@stud.vilniustech.lt>
2026-09-14 15:24:49 +0300

    Update LD1_9var_KarolisJuodzevicius.m
    Ld1

--- LD1/LD1_9var_KarolisJuodzevicius.m
+++ LD1/LD1_9var_KarolisJuodzevicius.m
@@ -2,8 +2,6 @@
 % Variantas: 9
 % Data: 2026-09-14
 
-N = 9;
-
 x = 1:32;
 y = x.^2;
 
@@ -13,5 +11,22 @@ xlabel('X-ai')
 ylabel('F_1 [-o-]  | F_2 [-x-]')
 
 help sin
-help linspace
+help linspace;
+%% 
+% Karolis Juodzevicius
+% Variantas: 9
+% Data: 2026-09-14
 
+N = 9;
+
+v = N + 1 : 0.5 : N + 4;
+
+A = N:N+8;
+A = reshape(A, 3, 3);
+
+A_a = A(3,2);
+A_b = A(2:3,1:2);
+A_c = A([1 3],[1 3]);
+
+v3 = v(1:3);
+A_nauja = [A; v3];
